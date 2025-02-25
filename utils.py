023958c2c0c4ab951e6aa3dd330f1bab25289934
WillemDeBock <162486876+WillemDeBock@users.noreply.github.com>
2025-02-25 23:32:41 +0100

    - Added more utility in utils.py - Created an index page, with bootsrap and css

--- utils.py
+++ utils.py
@@ -18,6 +18,7 @@ class TokenDashboard:
     def simple_request(self, endpoint, params=None):
         """Handles API requests dynamically with basic error handling."""
         url = f"{self.base_url}/{endpoint}"
+        print(f"Base URL: {url}")
         headers = {"accept": "application/json", "x-cg-demo-api-key": self.api_key}
 
         try:
@@ -44,8 +45,46 @@ class TokenDashboard:
 
     def supported_currencies_list(self):
         """This method should print a list of the top crypto currencies"""
-        supported_currencies_list = (self.simple_request("simple/supported_vs_currencies"))  # No extra params needed
+        supported_currencies_list = self.simple_request(
+            "simple/supported_vs_currencies"
+        )  # No extra params needed
         # print(len(supported_currencies_list))  # For some reason we only get 64 elements, which is annoying, might have to find another method to load the top 1k tokens ordered by MKTCAP
+
+        # Print the result
+        # print(supported_currencies_list)
+        # Not in use, bec shitty method, with no usecase
+
+    def get_top100_tokens(self):
+        params = {
+            "vs_currency": "usd",
+            "order": "market_cap_desc",
+            "per_page": 100,
+            "page": 1,
+            "price_change_percentage": "1h,24h,7d",
+        }
+
+        top100 = self.simple_request("coins/markets", params)
+
+        # Create a list to store all tokens' data
+        crypto_data = []
+
+        # Iterate over the results and append each token's data to the list
+        for x in top100:
+            token_data = {
+                "name": x["id"],
+                "sector": "None",
+                "price": x["current_price"],
+                "change_1h": x["price_change_percentage_1h_in_currency"],
+                "change_24h": x["price_change_percentage_24h_in_currency"],
+                "change_7d": x["price_change_percentage_7d_in_currency"],
+                "market_cap": x["market_cap"],
+                "volume_24h": x["total_volume"],
+            }
+            crypto_data.append(token_data)
+
+
+        print(crypto_data)  # For debuggin purposes
+        return crypto_data  # Return the complete list
 
 
 
@@ -53,4 +92,4 @@ class TokenDashboard:
 run = TokenDashboard()
 run.check_ping()
 run.supported_currencies_list()
-
+run.get_top100_tokens()
